55e3e14b5ee3f3dc20926d1459cbb1cd67190bfe
DeyanDanailov <stormbringer_ss@abv.bg>
2021-01-27 16:35:21 +0200

    up

--- SubqueriesAndJoins/GeographyDBProblems.sql
+++ SubqueriesAndJoins/GeographyDBProblems.sql
@@ -59,19 +59,38 @@ SELECT TOP(5) c.CountryName,
 	GROUP BY c.CountryName
 	ORDER BY HighestPeakElevation DESC, 
 				LongestRiverLength DESC, 
-				c.CountryName
+				c.CountryName;
 
 
-SELECT TOP(5) c.CountryName, 
-		MAX(p.Elevation) AS HighestPeakElevation,
-		MAX(r.Length) AS LongestRiverLength
-	FROM Countries AS c
-	LEFT JOIN CountriesRivers AS cr ON c.CountryCode = cr.CountryCode
-	LEFT JOIN Rivers AS r ON cr.RiverId = r.Id
-	LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
-	LEFT JOIN Mountains AS m  ON mc.MountainId = m.Id
-	LEFT JOIN Peaks AS p ON m.Id = p.MountainId
-	GROUP BY c.CountryName
-	ORDER BY HighestPeakElevation DESC, 
-				LongestRiverLength DESC, 
-				c.CountryName
+SELECT TOP (5) Country,
+               CASE
+                   WHEN PeakName IS NULL THEN '(no highest peak)'
+                   ELSE PeakName
+                   END AS [Highest Peak Name],
+               CASE
+                   WHEN Elevation IS NULL THEN 0
+                   ELSE Elevation
+                   END AS [Highest Peak Elevation],
+               CASE
+                   WHEN Mountain IS NULL THEN '(no mountain)'
+                   ELSE Mountain
+                   END AS Mountain
+FROM (
+         SELECT *,
+                DENSE_RANK() OVER (PARTITION BY Country ORDER BY Elevation DESC)
+                    AS PeakRank
+         FROM (SELECT CountryName     AS Country,
+                      p.PeakName,
+                      p.Elevation,
+                      M.MountainRange AS Mountain
+               FROM Countries AS c
+                        LEFT OUTER JOIN MountainsCountries MC
+                                        ON c.CountryCode = MC.CountryCode
+                        LEFT OUTER JOIN Mountains M
+                                        ON MC.MountainId = M.Id
+                        LEFT OUTER JOIN Peaks P
+                                        ON M.Id = P.MountainId)
+                  AS AllPeaksQuery)
+         AS PeakRankingQuery
+WHERE PeakRank = 1
+ORDER BY Country, [Highest Peak Name]
