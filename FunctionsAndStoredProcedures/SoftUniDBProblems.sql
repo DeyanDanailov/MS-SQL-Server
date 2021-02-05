41188e6c7221990748b87b7d05922c2839fd532a
DeyanDanailov <stormbringer_ss@abv.bg>
2021-02-05 13:36:50 +0200

    up

--- FunctionsAndStoredProcedures/SoftUniDBProblems.sql
+++ FunctionsAndStoredProcedures/SoftUniDBProblems.sql
@@ -55,4 +55,37 @@ END
 SELECT Salary, dbo.ufn_GetSalaryLevel(Salary) AS [Salary Level]
 	FROM Employees
 
-	ufn_IsWordComprised(@setOfLetters, @word) 
+
+CREATE PROC usp_EmployeesBySalaryLevel @LevelOfSalary NVARCHAR(7)
+	AS
+	SELECT FirstName, LastName
+		FROM
+		Employees
+		WHERE dbo.ufn_GetSalaryLevel(Salary) = @LevelOfSalary
+
+EXEC usp_EmployeesBySalaryLevel 'High'
+
+CREATE OR ALTER FUNCTION ufn_IsWordComprised(@setOfLetters varchar(max), @word varchar(max))
+    RETURNS bit AS
+BEGIN
+    DECLARE @WordLength int=LEN(@word);
+    DECLARE @Index int=1;
+
+    WHILE (@Index <= @WordLength)
+        BEGIN
+            IF (CHARINDEX(SUBSTRING(@word, @Index, 1), @setOfLetters)=0)
+                BEGIN
+                    RETURN 0
+                END
+
+            SET @Index+=1
+        END
+    RETURN 1
+END
+GO
+
+SELECT dbo.ufn_IsWordComprised('oistmiahf', 'Sofia')
+SELECT dbo.ufn_IsWordComprised('oistmiahf', 'halves')
+SELECT dbo.ufn_IsWordComprised('bobr', 'Rob')
+SELECT dbo.ufn_IsWordComprised('pppp', 'Guy')
+GO
